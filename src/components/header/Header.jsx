cfb13b4bff3c27c593d0c0129172b9bb0a8546e9
MNxMunno <bilolhuzayfa@gmmail.com>
2024-04-17 22:49:58 +0500

    5-Homework

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <nav>
+          <a href="#" className="logo">
+            <h1>SAMSUNG</h1>
+          </a>
+          <div className="nav__items"></div>
+          <div className="nav__btns"></div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Product from "../product/Product.jsx";
+
+const Main = () => {
+  return (
+    <main>
+      <Product />
+    </main>
+  );
+};
+
+export default Main;
