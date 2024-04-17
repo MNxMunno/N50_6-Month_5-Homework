97f08b6a525de1d0cc66c700bc2ef19521cb592c
MNxMunno <bilolhuzayfa@gmmail.com>
2024-04-18 00:06:14 +0500

    hali toliq emas :) :) :)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,35 @@
 import React from "react";
+import { IoMdSearch } from "react-icons/io";
+import { FiShoppingCart } from "react-icons/fi";
+import { FaRegUser } from "react-icons/fa";
 
 const Header = () => {
+  const items = [
+    "Shop",
+    "All",
+    "Mobile",
+    "TV & Audio",
+    "Appliance",
+    "Computing",
+    "Displays",
+    "Accessories",
+    "SmartThings",
+  ];
+
+  let item = items?.map((item, inx) => (
+    <a className="links" key={inx} href="#">
+      {item}
+    </a>
+  ));
+
+  //////////////////////////////////////
+
+  const btnItems = ["Explore", "Support", "For Business"];
+  let btnItem = btnItems?.map((el, inx) => (
+    <a key={inx} href="#">
+      {el}
+    </a>
+  ));
   return (
     <header>
       <div className="container">
@@ -8,8 +37,19 @@ const Header = () => {
           <a href="#" className="logo">
             <h1>SAMSUNG</h1>
           </a>
-          <div className="nav__items"></div>
-          <div className="nav__btns"></div>
+          <div className="nav__items">{item}</div>
+          <div className="nav__btns">
+            {btnItem}
+            <button>
+              <IoMdSearch className="search" />
+            </button>
+            <button>
+              <FiShoppingCart />
+            </button>
+            <button>
+              <FaRegUser />
+            </button>
+          </div>
         </nav>
       </div>
     </header>
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,7 +1,72 @@
 import React from "react";
 
 const Product = () => {
-  return <div>Product</div>;
+  const links = [
+    "Mobile & Computing",
+    "TV & Audio",
+    "Home Appliance",
+    "Samsung Live",
+    "For Business",
+  ];
+
+  let link = links?.map((el, inx) => (
+    <a href="#" key={inx}>
+      {el}
+    </a>
+  ));
+
+  ////////////////////////////////////////////
+  const data = [
+    {
+      id: 1,
+      img: "https://i.postimg.cc/yN3vPvJK/product-1.webp",
+      title: "Galaxy Book4 Ultra",
+      price: "500",
+    },
+    {
+      id: 2,
+      img: "https://i.postimg.cc/VLLyRPzc/product-2.webp",
+      title: "Galaxy Buds2 Pro",
+      price: "50 ",
+    },
+    {
+      id: 3,
+      img: "https://i.postimg.cc/sfmq1H7H/product-3.webp",
+      title: "Galaxy S24 Ultra",
+      price: "750",
+    },
+    {
+      id: 4,
+      img: "https://i.postimg.cc/Fsn6VfPj/product-4.webp",
+      title: "Galaxy Tab S9 Series",
+      price: "800",
+    },
+    {
+      id: 5,
+      img: "https://i.postimg.cc/L6hC59Hh/product-5.jpg",
+      title: "Galaxy Watch6 Series",
+      price: "250",
+    },
+  ];
+
+  let card = data?.map((el) => (
+    <div className="card" key={el.id}>
+      <img src={el.img} alt="img" />
+      <h3>{el.title}</h3>
+      <h4>{el.price}$</h4>
+    </div>
+  ));
+  return (
+    <section className="product">
+      <div className="container">
+        <div className="product__content">
+          <h1 className="pro_title">Shop all latest offers and innovations</h1>
+          <div className="hero-links">{link}</div>
+          <div className="product__cards">{card}</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Product;
